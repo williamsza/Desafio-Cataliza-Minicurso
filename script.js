3ae6549d508691342243cd312362dc0ef337bd03
williamsza <william.sza@outlook.com>
2022-01-16 19:47:33 -0300

    Adiconar bonus traducao da condicao

--- script.js
+++ script.js
@@ -4,6 +4,17 @@ const nomeDoPersonagem = document.querySelector('#nome');
 const especie = document.querySelector('#especie');
 const condicao = document.querySelector('#status');
 
+traduzirCondição = (data) => {
+if(data.status) == 'unknow'){
+     return "Não sabemos";
+     }else if(data.status == 'Alive'){
+     return 'Sim';
+     }else {
+return 'Não. Está morto';
+
+    }
+
+}
 gerarValorAleatorio = () => {
   return Math.floor(Math.random() * 3);
 
@@ -18,21 +29,20 @@ gerarValorAleatorio = () => {
             method:'GET',
             headers: {
             Accept: 'application/json',
-                 "Content-type": 'application/json'
+           "Content-type": 'application/json'
 
    }
 
- 
    }).then((Response) => Response.json()).then((data) => {
          imagem.src = data.image;
          imagem.alt = data.name;
          nomeDoPersonagem.innerHTML = data.name;
          especie.innerHTML = data.especies;
          condicao.innerHTML = data.status;
+         condicao.innerHTML = traduzirCondição(data);
 
 });
+
 }
-
-
 
 botao.onclick = pegarPersonagem;
